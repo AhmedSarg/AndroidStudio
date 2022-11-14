f3474446113bfdad38659ea1f11fe0a39abf9cfb
AhmedSarg <xsarg22@gmail.com>
2022-11-14 22:46:31 +0200

    new

---
+++ CustomFanController/app/src/main/java/com/example/android/customfancontroller/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.customfancontroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(sa
+                -+vedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
